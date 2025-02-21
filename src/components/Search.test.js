06631e397b42dca4d6a80255696c25faf1a9760d
Manikanta662001 <gundlurimanikanta142@gmail.com>
2025-02-21 09:16:19 +0530

    used rerender in testcase

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -1,27 +1,44 @@
-import { fireEvent, render, screen, } from "@testing-library/react";
-import userEvent from '@testing-library/user-event'
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Search from "./Search";
 
-describe('Tests Search compo',()=>{
-    test('calls the onChange callback handler',()=>{
-        const onChange = jest.fn()
-        render(
-            <Search value={''} onchange={onChange}>
-            Search :
-            </Search>
-        )
-        fireEvent.change(screen.getByRole('textbox'),{target:{value:'hello'}})
-        expect(onChange).toHaveBeenCalledTimes(1)
-    })
-    test('calls the onChange callback handler by userEvent',async()=>{
-        const onChange = jest.fn()
-        render(
-            <Search value={''} onchange={onChange}>
-            Search :
-            </Search>
-        )
-        await userEvent.type(screen.getByRole('textbox'),'Javascript')
-        expect(onChange).toHaveBeenCalledTimes(10)
-    })
-
-})
+describe("Tests Search compo", () => {
+  test("calls the onChange callback handler", () => {
+    const onChange = jest.fn();
+    render(
+      <Search value={""} onchange={onChange}>
+        Search :
+      </Search>
+    );
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: "hello" },
+    });
+    expect(onChange).toHaveBeenCalledTimes(1);
+  });
+  test("calls the onChange callback handler by userEvent", async () => {
+    const onChange = jest.fn();
+    render(
+      <Search value={""} onchange={onChange}>
+        Search :
+      </Search>
+    );
+    await userEvent.type(screen.getByRole("textbox"), "Javascript");
+    expect(onChange).toHaveBeenCalledTimes(10);
+  });
+  test("with new Props using rerender", async () => {
+    const onChange = jest.fn();
+    const { rerender } = render(
+      <Search value={"hello"} onchange={onChange}>
+        Search :
+      </Search>
+    );
+    const inputBox = screen.getByRole("textbox");
+    expect(inputBox).toHaveDisplayValue("hello");
+    rerender(
+      <Search value={"world"} onchange={onChange}>
+        Search :
+      </Search>
+    );
+    expect(inputBox).toHaveDisplayValue("world");
+  });
+});
